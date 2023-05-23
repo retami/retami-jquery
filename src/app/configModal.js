5c259a23e02a71651be157ae28685ab8186e2f52
dev3139 <psatchy@gmail.com>
2023-05-23 11:34:15 +0200

    use const, init empty callable

--- src/app/configModal.js
+++ src/app/configModal.js
@@ -3,15 +3,15 @@ import "jquery-ui/ui/widgets/slider";
 
 export default {
 
-    callable: null,
+    callable: () => {},
 
     bind(callable) {
         this.callable = callable;
     },
 
     show: function (game) {
-        let config = $('.modal');
-        config.html(this.html2(game));
+        const config = $('.modal');
+        config.html(this.html(game));
 
         $('.pref-colors button').eq(game.numberOfColors - 2).addClass('selected');
         $('.pref-pins button').eq(game.numberOfPins - 2).addClass('selected');
@@ -35,40 +35,6 @@ export default {
             $("#config_guesses").val($(e.target).text());
         });
 
-        /*
-                    $("#config_colors_slider").slider({
-                        value: game.numberOfColors,
-                        min: 2,
-                        max: 7,
-                        step: 1,
-                        slide: function (event, ui) {
-                            $("#config_colors").val(ui.value);
-                            $("#config_colors_text").text(ui.value);
-                        }
-                    });
-
-                    $("#config_pins_slider").slider({
-                        value: game.numberOfPins,
-                        min: 2,
-                        max: 5,
-                        step: 1,
-                        slide: function (event, ui) {
-                            $("#config_pins").val(ui.value);
-                            $("#config_pins_text").text(ui.value);
-                        }
-                    });
-
-                    $("#config_guesses_slider").slider({
-                        value: game.maxAttempts,
-                        min: 2,
-                        max: 12,
-                        step: 1,
-                        slide: function (event, ui) {
-                            $("#config_guesses").val(ui.value);
-                            $("#config_guesses_text").text(ui.value);
-                        }
-                    });
-        */
         config.css('display', 'block');
         $('.modal-button').on('click', () => {
             this.callable();
@@ -76,31 +42,8 @@ export default {
             $('.modal-button').off('click');
         });
     },
+
     html: function (game) {
-        return `<div id='config-modal'>
-                <div id='config_dialog'>
-                <label id='colors_label'>Colors: </label>
-                <div id='config_colors_text'>${game.numberOfColors}</div>
-                <div id='config_colors_slider'></div>
-
-                <label id='pins_label'>Pins: </label>
-                <div id='config_pins_text'>${game.numberOfPins}</div>
-                <div id='config_pins_slider'></div>
-
-                <label id='guesses_label'>Guesses: </label>
-                <div id='config_guesses_text'>${game.maxAttempts}</div>
-                <div id='config_guesses_slider'></div>
-                <form>
-                <input type='hidden' value='${game.numberOfColors}' id='config_colors' />
-                <input type='hidden' value='${game.numberOfPins}' id='config_pins' />
-                <input type='hidden' value='${game.maxAttempts}' id='config_guesses' />
-                </form>
-                </div>
-                <div class="modal-button">OK</div>
-                `;
-    },
-
-    html2: function (game) {
         return `<div id='config-modal'>
                 <div id='config_dialog'>
                 
