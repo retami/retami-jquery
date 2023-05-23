bfee1f5754d21cda545c385bf598b92794b9e102
dev3139 <psatchy@gmail.com>
2023-05-23 11:26:50 +0200

    improve description

--- cypress/e2e/Hint button click.cy.js
+++ cypress/e2e/Hint button click.cy.js
@@ -1,18 +1,23 @@
 describe('Hint button click', () => {
 
-    it('Hint button clicks show secret', () => {
+    it('Clicking the hint button reveals one code hole at a time', () => {
         cy.visit('/');
-        cy.get('.start-button').click();
+        cy.get('.start-button').should('exist').click();
 
         cy.get('[class="code-hole covered"]').should('have.length', 4);
+
         cy.get('.hint-button').click();
         cy.get('[class="code-hole covered"]').should('have.length', 3);
+
         cy.get('.hint-button').click();
         cy.get('[class="code-hole covered"]').should('have.length', 2);
+
         cy.get('.hint-button').click();
         cy.get('[class="code-hole covered"]').should('have.length', 1);
+
         cy.get('.hint-button').click();
         cy.get('[class="code-hole covered"]').should('have.length', 1);
+
         cy.get('.hint-button').click();
         cy.get('[class="code-hole covered"]').should('have.length', 1);
     });
