603ef8f5705e4a96323f99720cbe6e8cc9325564
dev3139 <psatchy@gmail.com>
2023-05-23 10:00:59 +0200

    lint html

--- src/app/helpModal.js
+++ src/app/helpModal.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 export default {
 
     helpHtml: `<div id="help-modal">
-        <p >The goal of the game is to crack the code in as few attempts as possible.
+        <p>The goal of the game is to crack the code in as few attempts as possible.
         The code is made up of 4 colors. Each color can be used multiple times. After each guess,
         you will get feedback in the form of black and white pins. A black pin means that you have
         guessed a color and its position correctly. A white pin means that you have guessed a color
