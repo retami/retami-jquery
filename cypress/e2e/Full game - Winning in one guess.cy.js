e6886436fb5fd8d5645a78489a83bbfe5f81e148
calcdev248 <calcdev248@gmail.com>
2023-05-27 13:51:06 +0200

    refactor and enhance

--- cypress/e2e/Full game - Winning in one guess.cy.js
+++ cypress/e2e/Full game - Winning in one guess.cy.js
@@ -11,24 +11,51 @@ describe('Full game - Winning in one guess', () => {
             secret = win.app.secret;
             colors = win.app.game.colors;
         }).then(() => {
-            for (let hole = 0; hole < 4; hole++) {
-                const key = colors.findIndex((element) => element === secret.code[hole]);
-                cy.log(key.toString());
-                cy.get('.current').find('.guess-holes').find('.guess-hole').eq(hole).click();
-                cy.get(`.color-${key + 1}`).should('be.visible');
-                cy.get(`.color-${key + 1}`).forceClick();
-                cy.get(`.color-${key + 1}`).should('not.be.visible');
-                cy.get('.current').find('.guess-holes').find('.guess-hole').eq(hole).should("have.css", "background-color", colors[key]);
-            }
+            let code = Array.from(secret.code, el => colors.findIndex((element) => element === el));
+
+            setGuess(colors, [ other(code, 5), other(code, 5), other(code, 5), other(code, 5)]);
+            assertFeedback(0, ['none', 'none', 'none', 'none']);
+            setGuess(colors, [ code[0], other(code, 5), other(code, 5), other(code, 5)]);
+            assertFeedback(1, ['black', 'none', 'none', 'none']);
+            setGuess(colors, [ code[0], code[1], other(code, 5), other(code, 5)]);
+            assertFeedback(2, ['black', 'black', 'none', 'none']);
+            setGuess(colors, [ code[0], code[1], code[2], other(code, 5)]);
+            assertFeedback(3, ['black', 'black', 'black', 'none']);
+            setGuess(colors, code);
+            assertFeedback(4, ['black', 'black', 'black', 'black']);
+            cy.get('#win-modal').should('be.visible').click().should('not.be.visible');
         });
 
-        cy.get('.check-button').click();
+        function other(set, length) {
+            let candidates = Array.from(new Array(length).keys()).filter(val => ! set.includes(val));
+            return candidates[Math.floor(Math.random()*candidates.length)];
+        }
 
-        cy.get('.feedback-hole').eq(0).should('have.class', 'black-feedback');
-        cy.get('.feedback-hole').eq(1).should('have.class', 'black-feedback');
-        cy.get('.feedback-hole').eq(2).should('have.class', 'black-feedback');
-        cy.get('.feedback-hole').eq(3).should('have.class', 'black-feedback');
-
-        cy.get('#win-modal').should('be.visible').click().should('not.be.visible');
+        function setGuess(colors, guess) {
+            for (let hole = 0; hole < guess.length; hole++) {
+                cy.log(guess[hole].toString());
+                cy.get('.current').find('.guess-holes').find('.guess-hole').eq(hole).click();
+                cy.get(`.color-${guess[hole] + 1}`).should('be.visible');
+                cy.get(`.color-${guess[hole] + 1}`).forceClick();
+                cy.get(`.color-${guess[hole] + 1}`).should('not.be.visible');
+                cy.get('.current').find('.guess-holes').find('.guess-hole').eq(hole).should("have.css", "background-color", colors[guess[hole]]);
+                cy.get('.check-button').click();
+            }
+        }
     });
+
+    function assertFeedback(row, pins) {
+        for(let i= 0; i < pins.length; i++){
+            if(pins[i] === 'black') {
+                cy.get('.feedback-hole').eq(row*pins.length + i).should('have.class', 'black-feedback');
+            }
+            if(pins[i] === 'white') {
+                cy.get('.feedback-hole').eq(row*pins.length + i).should('have.class', 'white-feedback');
+            }
+            if(pins[i] === 'none') {
+                cy.get('.feedback-hole').eq(row*pins.length + i).should('not.have.class', 'black-feedback');
+                cy.get('.feedback-hole').eq(row*pins.length + i).should('not.have.class', 'white-feedback');
+            }
+        }
+    }
 });
